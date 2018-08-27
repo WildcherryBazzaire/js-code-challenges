4e47878c3ac1013b69c8dbaff8be6269ff055854
Alex <eieghtbitdolphin@gmail.com>
2018-08-26 23:50:09 -1000

    gottem

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,16 @@
 module.exports = function acceptableSequence(str) {
+    var letters = "abcdefghijklmnopqrstuvqxyz";
+    for(var i=0; i < str.length; i++) {
+        if ( (letters.indexOf(str[i]) !== -1) && (i !== 0) && (i !== str.length-1) ) { //checks if it is a letter by finding a index in the letters variable
+            
+            if( (str[i-1] === "+") && (str[i+1] === "+") ) { //if the next characters surrounding it are +'s
+                return true;
+            } 
 
+        } else if( (letters.indexOf(str[i]) !== -1) && ((i === 0) || (i === str.length-1)) ) {
+            return false;
+        } 
+    
+    }
+    return false
 }
